795a3465d594d24882a4f89c08089dd52fa1de3d
jessicayjung <jessica.yjung75@gmail.com>
2020-04-25 00:14:31 -0700

    Commit repo
    Added project file

---
+++ Assets/scripts/Follow.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class Follow : MonoBehaviour
+{
+    public GameObject target;
+    public float followDistance;
+    public float cameraFollowSpeed = 0.1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 targetPosition = target.transform.position;
+
+        Vector3 followVector = transform.forward * -followDistance;
+
+        transform.position = targetPosition + followVector;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 targetPosition = target.transform.position;
+
+        Vector3 followVector = transform.forward * -followDistance;
+
+        Vector3 cameraTargetPosition = targetPosition + followVector;
+
+        if (Input.GetKey(KeyCode.A))
+        { 
+            gameObject.transform.RotateAround(target.transform.position, Vector3.up, target.transform.eulerAngles.y);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            gameObject.transform.RotateAround(target.transform.position, Vector3.up, -target.transform.eulerAngles.y);
+        }
+
+        transform.position = Vector3.Lerp(transform.position, cameraTargetPosition, cameraFollowSpeed * Time.deltaTime);
+
+
+    }
+}
---
+++ Assets/scripts/Move.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Threading;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+   public Vector3 speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 currentSpeed = Vector3.zero;
+        if (Input.GetKey(KeyCode.A))
+        {
+            gameObject.transform.RotateAround(transform.position, Vector3.up, -speed.y * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            gameObject.transform.RotateAround(transform.position, Vector3.up, speed.y * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            currentSpeed.z = speed.z; 
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            currentSpeed.z = -speed.z;
+        }
+        gameObject.transform.Translate(currentSpeed * Time.deltaTime);
+    }
+}
